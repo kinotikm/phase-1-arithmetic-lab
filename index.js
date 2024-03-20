c0ec9f5a814985db87a3dd1c403113fba9aac28c
kinotikm <cloutkevin254@gmail.com>
2024-03-20 09:09:54 +0300

    mdify index.js

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+console.log("multiply:", multiply); 
 
-// Write your code here
+
+const random = Math.floor(Math.random() * 100) + 1;
+console.log("random:", random); 
+
+
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+console.log("mod:", mod);  
+
+
+const max = Math.max(5, 10, 15, 20);
+console.log("max:", max); 
+
